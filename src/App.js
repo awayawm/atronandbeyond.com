eccb09446c4ca3aac41256de56a0abbc0a15e3a1
lakeoftea <lakeoftea@gmail.com>
2021-06-26 14:26:16 -0500

    Put navbar back into app.js for now

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import * as data from "./data"
-import Header from './Components/Header'
-import Articles from './Components/Articles'
-// import Footer from './Components/Footer'
-import { CssBaseline, Container } from '@material-ui/core'
-import "./css/App.css";
-import { Helmet } from 'react-helmet'
-import '@fontsource/baloo-bhai-2/400.css'
-import '@fontsource/baloo-bhai-2/500.css'
-import '@fontsource/baloo-bhai-2/700.css'
+import Header from "./Components/Header"
+import Articles from "./Components/Articles"
+import { CssBaseline, Container } from "@material-ui/core"
+import "./css/App.css"
+import { Helmet } from "react-helmet"
+import "@fontsource/baloo-bhai-2/400.css"
+import "@fontsource/baloo-bhai-2/500.css"
+import "@fontsource/baloo-bhai-2/700.css"
+import { BrowserRouter, Link } from "react-router-dom"
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { ...data }
+	}
 
-  constructor(props) {
-    super(props)
-    this.state = { ...data };
-  }
-
-  render() {
-    return (
-      <>
-        <Helmet>
-          <title>atronandbeyond.com: Beats and Code</title>
-          <body className="body" />
-        </Helmet>
-        <CssBaseline />
-        <Container>
-          <Header header={this.state.header} tags={this.state.tags} />
-          <Articles articles={this.state.articles} tags={this.state.tags} />
-          {/* <Footer footer={this.state.footer} /> */}
-        </Container>
-      </>
-    );
-  }
+	render() {
+		return (
+			<>
+				<Helmet>
+					<title>atronandbeyond.com: Beats and Code</title>
+					<body className="body" />
+				</Helmet>
+				<CssBaseline />
+				<Container>
+					<Header header={this.state.header} tags={this.state.tags} />
+					<BrowserRouter>
+						<div className="navbar">
+							<ul>
+								{this.state.tags
+									.filter((tag) => tag.enabled)
+									.map((tag, i) => (
+										<li key={i}>
+											{tag.icon} <Link to={tag.link}>{tag.name}</Link>{" "}
+										</li>
+									))}
+							</ul>
+						</div>
+					</BrowserRouter>
+					<Articles articles={this.state.articles} tags={this.state.tags} />
+				</Container>
+			</>
+		)
+	}
 }
 
-export default App;
+export default App
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,28 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import '../css/Header.css'
-import Nav from '../Components/Nav'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import "../css/Header.css"
 
 export default class Header extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (<header>
-            <h1>
-                {this.props.header.title}
-            </h1>
-            <div>
-                {this.props.header.tagline}
-            </div>
-            <Nav tags={this.props.tags} />
-            <div>
-                <a href={this.props.header.github}>Github</a>
-            </div>
-            <div>
-                <a href={this.props.header.linkedin}>Linkedin</a>
-            </div>
-        </header>)
-    }
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<>
+				<header>
+					<h1>{this.props.header.title}</h1>
+					<div>{this.props.header.tagline}</div>
+				</header>
+				<div className="links">
+					<a href={this.props.header.github}>Github</a>
+					<a href={this.props.header.linkedin}>Linkedin</a>
+				</div>
+			</>
+		)
+	}
 }
 
 Header.propTypes = {
-    tags: PropTypes.array,
-    header: PropTypes.object
-};
+	tags: PropTypes.array,
+	header: PropTypes.object,
+}
