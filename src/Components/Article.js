f710789a7f4b3a9d0aeee465b369d0f82bdfdf59
awayawm <aaron@atronandbeyond.com>
2021-06-26 23:37:33 -0500

    Begin article

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,7 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
+import { Paper } from "@material-ui/core";
+import PropTypes from "prop-types"
+import { useParams } from "react-router-dom"
+import '../css/Article.css'
 
-export default class Article extends Component{
-    render(){
-        return(<>{`test`}</>)
-    }
+let Article = (props) => {
+    let { name } = useParams();
+    let article = props.articles.find(article => article.title.toLowerCase().replace(/\s/g, '-') == name)
+    console.log(`name ${name}, articles: ${JSON.stringify(article)}`)
+
+    return (<Paper className="article">
+        <h1>{article.title}</h1>
+        <p
+            dangerouslySetInnerHTML={{
+                __html: article.content
+            }}
+        />
+    </Paper>)
+}
+
+export default Article;
+
+Article.propTypes = {
+    articles: PropTypes.array,
+    tags: PropTypes.array,
 }
--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -5,19 +5,23 @@ import { Card, CardContent, Grid } from "@material-ui/core"
 import { useParams } from "react-router-dom"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
+import { Link } from 'react-router-dom'
 
-let Article = (props) => {
+let Articles = (props) => {
+	let getLink = (link) => {
+		return `/article/${link.toLowerCase().replace(/\s/g, '-')}`;
+	}
 	let { name } = useParams()
 	let articles
-    let title;
+	let title;
 	if (name != undefined) {
-		let id = props.tags.filter((tag) => tag.name == _.startCase(name))[0].id
+		let id = props.tags.find((tag) => tag.name == _.startCase(name)).id
 		// console.log(`name: ${name}, id: ${id}, article.tags ${JSON.stringify(props.articles)}`)
 		articles = props.articles.filter((article) => article.tags.includes(id))
-        title = `Atronandbeyond.com: ${_.startCase(name)}`
+		title = `Atronandbeyond.com: ${_.startCase(name)}`
 	} else {
 		articles = props.articles
-        title = `Atronandbeyond.com: Beats and Code`
+		title = `Atronandbeyond.com: Beats and Code`
 	}
 	return (
 		<>
@@ -27,49 +31,49 @@ let Article = (props) => {
 			<div className="articleGrid">
 				<Grid container spacing={2}>
 					{//article is enabled
-                    articles
-						.filter((article) => article.enabled)
-						// has at least one tag that is enabled
-						.filter((article) => {
-							return (
-								_.intersection(
-									article.tags,
-									props.tags
-										.filter((tag) => tag.enabled)
-										.map((value) => value.id)
-								).length > 0
-							)
-						})
-						.map((article, i) => (
-							<Grid item lg={6} key={i}>
-								<Card className="article">
-									<CardContent className="cardContent">
-										<h1 className="icon">{article.title}</h1>
-										<div className="icon">
-											{article.tags.map(
-												(articleTag) =>
-													_.find(props.tags, (tag) => articleTag == tag.id).icon
-											)}
-										</div>
-										<p
-											dangerouslySetInnerHTML={{
-												__html: _.truncate(article.content, { length: 250 }),
-											}}
-										/>
-										<div>{article.date}</div>
-									</CardContent>
-								</Card>
-							</Grid>
-						))}
+						articles
+							.filter((article) => article.enabled)
+							// has at least one tag that is enabled
+							.filter((article) => {
+								return (
+									_.intersection(
+										article.tags,
+										props.tags
+											.filter((tag) => tag.enabled)
+											.map((value) => value.id)
+									).length > 0
+								)
+							})
+							.map((article, i) => (
+								<Grid item lg={6} key={i}>
+									<Card className="article">
+										<CardContent className="cardContent">
+											<h1><Link to={getLink(article.title)}>{article.title}</Link></h1>
+											<div className="icon">
+												{article.tags.map(
+													(articleTag) =>
+														_.find(props.tags, (tag) => articleTag == tag.id).icon
+												)}
+											</div>
+											<p
+												dangerouslySetInnerHTML={{
+													__html: _.truncate(article.content, { length: 250 }),
+												}}
+											/>
+											<div>{article.date}</div>
+										</CardContent>
+									</Card>
+								</Grid>
+							))}
 				</Grid>
 			</div>
 		</>
 	)
 }
 
-export default Article
+export default Articles
 
-Article.propTypes = {
+Articles.propTypes = {
 	articles: PropTypes.array,
 	tags: PropTypes.array,
 }
