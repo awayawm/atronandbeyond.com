5bee2da2d58ef4175b9426d2d157afeb4d98c19d
awayawm <lakeoftea@gmail.com>
2021-09-20 21:27:46 -0500

    add react-spring to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "../css/Header.css"
+import {useSpring, animated, config} from "react-spring";
+import {Typography} from "@material-ui/core";
 
 let Header = (props) => {
-	return(
-			<>
-				<header>
-					<h1>{props.header.title}</h1>
-					<div>{props.header.tagline}</div>
-				</header>
-			</>
-		)
+    let headerProps = useSpring({
+        to: {opacity: 1},
+        from: {opacity: 0},
+        config: config.slow
+})
+    const AnimatedTypography = animated(Typography)
+    return (
+        <>
+            <header>
+                <AnimatedTypography variant="h3" style={headerProps}>{props.header.title}</AnimatedTypography>
+                <animated.div style={headerProps}>{props.header.tagline}</animated.div>
+            </header>
+        </>
+    )
 }
 
 Header.propTypes = {
-	tags: PropTypes.array,
-	header: PropTypes.object,
+    tags: PropTypes.array,
+    header: PropTypes.object,
 }
 
 export default Header
